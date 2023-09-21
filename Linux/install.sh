a4d6f929aa60510ed4b72e84a632971de713a5bd
Lrdsnow <63671193+Lrdsnow@users.noreply.github.com>
2023-09-21 14:07:24 -0700

    Update install.sh

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,3 +1,3 @@
-curl -s --compressed "https://lrdsnow.github.io/lrdsnows-repo/key.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/my_ppa.gpg >/dev/null
-sudo curl -s --compressed -o /etc/apt/sources.list.d/lrdsnowsrepo.list "https://lrdsnow.github.io/lrdsnows-repo/lrdsnowsrepo.list"
+curl -s --compressed "https://lrdsnow.github.io/lrdsnows-repo/Linux/key.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/my_ppa.gpg >/dev/null
+sudo curl -s --compressed -o /etc/apt/sources.list.d/lrdsnowsrepo.list "https://lrdsnow.github.io/lrdsnows-repo/Linux/lrdsnowsrepo.list"
 sudo apt update
